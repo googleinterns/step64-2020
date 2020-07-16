1df18001267d37e78e61b8032d829853311928d6
Shivani Ghanta <shivani.ghanta@gmail.com>
2020-07-16 04:24:53 +0000

    deleted unused imports

--- src/main/java/com/google/sps/servlets/DataServlet.java
+++ src/main/java/com/google/sps/servlets/DataServlet.java
@@ -44,7 +44,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-// import javax.xml.ws.http.HTTPException;
 import org.json.simple.JSONObject;
 
 /**

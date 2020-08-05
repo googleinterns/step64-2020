c4ed1d757ad29cf911768a245c4826fded4f9de1
Jonathan <jonathananer@google.com>
2020-08-05 21:23:47 +0000

    Fix lint formatting

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -18,7 +18,7 @@ let sortType = 'none';
 
 function addThreads() {
   const url = `/videos-sentiment?currentPage=${currentPage}&postPerPage=${
-      postPerPage}&sortType=${sortType}`;
+    postPerPage}&sortType=${sortType}`;
   fetch(url).then((response) => response.json()).then((threadInfoList) => {
     const threadList = document.getElementById('thread-container');
     threadList.innerHTML = '';
